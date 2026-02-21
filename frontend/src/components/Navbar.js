54bd594bb828760d088056cd2c54da8174e14b95
nikhilsquarrootz <nikhil.singh@squarerootz.com>
2026-02-21 09:09:13 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -27,12 +27,7 @@ export default function Navbar() {
     <nav className="fixed top-5 left-1/2 -translate-x-1/2 z-50 w-[92%] md:w-auto" data-testid="navbar">
       <div className="glass-navbar rounded-full px-4 md:px-6 h-14 flex items-center gap-2 md:gap-6">
         <Link to="/" className="font-heading font-extrabold text-xl tracking-tight text-Squarerootz-black mr-2 md:mr-6" data-testid="nav-logo">
-          <img 
-    src="/images/sqrtlogo.png" 
-    alt="Squarerootz Logo" 
-   className="w-6 h-6 rounded-full object-cover"
-  /><span className="leading-none">Squarerootz</span>
- 
+ Squarerootz
         </Link>
 
         {/* Desktop Links */}
